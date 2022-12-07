e6d540ecc14d076a21cd18dde4dd3c8246a49158
ekremeraykaya <ekremeraykaya@outlook.com>
2022-12-07 21:25:41 +0300

    Ders ornegi gonderiliyor

---
+++ OgrenciWebServisi/src/main/java/tr/edu/medipol/OgrenciWebServisi/OgrenciRestApi.java
@@ -0,0 +1,31 @@
+package tr.edu.medipol.OgrenciWebServisi;
+
+import java.util.*;
+
+import org.springframework.web.bind.annotation.*;
+
+// http://localhost:8080/ogrenci/listele
+
+@RestController				//   Rest web servis sinifi ozelligi kazandiriyoruz.
+@RequestMapping("/ogrenci") // "/ogrenci" ile baslayan web isteklerini bu sınıfa yönlendirir.
+public class OgrenciRestApi {
+	
+	private static final List<String> OGRENCILER = new ArrayList<>();
+	
+	
+	// statik blok
+	{
+		OGRENCILER.add("Jane");
+		OGRENCILER.add("Joe");
+
+	}
+	
+	// GET - veri almak -> kısaca
+	// POST - veri göndermek -> kısaca
+	
+	@GetMapping("/listele")
+	public List<String> listele() {
+		return OGRENCILER;
+	}
+
+}
